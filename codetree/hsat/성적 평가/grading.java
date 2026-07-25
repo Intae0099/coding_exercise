4a68389392932daf47603baaa6a22e8eda6802a4
TaeIn <81225723+Intae0099@users.noreply.github.com>
2026-07-25 15:03:34 +0900

    [Passed] 성적 평가 (772ms, 38MB)

--- codetree/hsat/성적 평가/grading.java
+++ codetree/hsat/성적 평가/grading.java
@@ -2,71 +2,57 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static class Node implements Comparable<Node>{
-        int idx, num;
-        Node(int idx, int num){
-            this.idx = idx;
-            this.num = num;
-        }
 
-        @Override
-        public int compareTo(Node o){
-            return Integer.compare(o.num, this.num);
-        }
-    }
     static int N;
-    static int[] sum_arr;
+    static int[] rank;
+    static int[] total_rank;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        
-        sum_arr = new int[N];
-        for(int i = 0; i < 3; i++){
+        total_rank = new int[3001];
+        int[] total_people = new int[N];
+        for (int i = 0; i < 3; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            ArrayList<Node> arr = new ArrayList<>();
-            for(int j = 0; j < N; j++){
+            rank = new int[1001];
+            int[] people = new int[N];
+            for (int j = 0; j < N; j++) {
                 int num = Integer.parseInt(st.nextToken());
-                sum_arr[j] += num;
-                arr.add(new Node(j, num));
+                people[j] = num;
+                total_people[j] += num;
             }
-            rank(arr);
+            cal_rank(people, rank);
             sb.append("\n");
+
         }
 
-        ArrayList<Node> result = new ArrayList<>();
-        for(int i = 0; i < N; i++) result.add(new Node(i, sum_arr[i]));
-        
-        rank(result);
+        cal_rank(total_people, total_rank);
+
         System.out.println(sb);
+
     }
 
-    public static void rank(ArrayList<Node> arr){
-        Collections.sort(arr);
-        int[] arr_rank = new int[N];
-        Node node = arr.get(0);
-        int now = node.idx;
-        arr_rank[now] = 1;
-        int prev = node.num;
-        int prev_idx = now;
-        for(int i = 1; i < N; i++){
-            node = arr.get(i);
-            int now_num = node.num;
-            int now_idx = node.idx;
-            if(prev == now_num){
-                arr_rank[now_idx] = arr_rank[prev_idx];
-            }
-            else{
-                arr_rank[now_idx] = i + 1;
-            }
-            prev = now_num;
-            prev_idx = now_idx;
-
+    public static void cal_rank(int[] people, int[] rank){
+        int[] temp = new int[N];
+        for (int i = 0; i < N; i++) {
+            temp[i] = people[i];
         }
 
-        for(int i : arr_rank){
-            sb.append(i).append(" ");
+        Arrays.sort(temp);
+        int cnt = 1;
+        for (int i = N-1; i >= 0 ; i--) {
+            int now_rank = temp[i];
+            if(rank[now_rank] == 0) rank[now_rank] = cnt;
+            cnt++;
+        }
+
+        print(people, rank);
+    }
+
+    public static void print(int[] people, int[] rank){
+        for (int i = 0; i < N; i++) {
+            sb.append(rank[people[i]]).append(" ");
         }
     }
 }
